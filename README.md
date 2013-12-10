69f9970a61f3c3a2d7abe6b2350fc65a832f06f5
SG <sy1840@gmail.com>
2013-12-10 12:50:21 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Vagrant Test App
+
+This was to test using Vagrant for a local dev environment.
+
+![Screenshot](https://raw.github.com/kwick6/Vagrant-Setup-Test-App/master/app/assets/images/vagrant-running.png)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6d4808c73d80b20f0391528a9bcd0b0180f652626d4c78c309421af71c0c588212ff325f5a3a228239be36dffa6d2527c9c67e329fe8064182aebe8bd8f288b3'
